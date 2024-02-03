e40cbab7f679623f6204e17f4f8b38fba8d69fb9
Gabriel Tomaz <gabriel97tomaz@hotmail.com>
2024-02-03 14:30:39 -0300

    adicionando designSystem

---
+++ src/tokens/colors/colors.ts
@@ -0,0 +1,70 @@
+export const colors = {
+  brandColors:{
+    colorPrimaryLight: '#F3C96F',
+    colorPrimaryMain: '#F1BC4B',
+    colorPrimaryDark: '#A88334',
+    colorPrimaryContrast: '#211E1E',
+    colorPrimaryBackground: '#FDF4E3',
+
+    colorSecondaryLight: '#F38F6F',
+    colorSecondaryMain: '#F1734B',
+    colorSecondaryDark: '#A85034',
+    colorSecondaryContrast: '#211E1E',
+    colorSecondaryBackground: '#FDE9E3',
+
+    colorTertiaryLight: '#9E604C',
+    colorTertiaryMain: '#863920',
+    colorTertiaryDark: '#5D2716',
+    colorTertiaryContrast: '#F0EFEE',
+    colorTertiaryBackground: '#F4E5E1 ',
+
+
+  },
+  feedbackColors:{
+    colorErrorLight: '#EF5350',
+    colorErrorMain: '#D32F2F',
+    colorErrorDark: '#C62828',
+    colorErrorContrast: '#FFFFFF',
+    colorErrorBackground: '#FFDDEE',
+
+    colorSuccessLight: '#4CAF50',
+    colorSuccessMain: '#2E7D32',
+    colorSuccessDark: '#1B5E20',
+    colorSuccessContrast : '#FFFFFF',
+    colorSuccessBackground: '#EDF7ED',
+
+    colorWarningLight: '#FF9800',
+    colorWarningMain: '#ED6C02',
+    colorWarningDark: '#E65100',
+    colorWarningContrast: '#211E1E',
+    colorWebkitURLarningBackground: '#FFF4E5',
+
+   
+
+    colorInfoLight: '#03A9F4',
+    colorInfoMain: '#0288D1',
+    colorInfoDark: '#01579B',
+    colorInfoContrast: '#FFFFFF',
+    colorInfoBackground: '#E5F6FD',
+  },
+
+  baseColors:{
+    colorBaseDarkSecondary: '#565454',
+    colorBaseDarkMain: '#333333',
+    colorBaseDarkHighlight: '#211E1E',
+    colorBaseDarkContrast: '#FFFFFF',
+    colorBaseDarkContrastDisabled:'#AEAEAE',
+
+    colorBaseLightSecondary: '#F5F5F5',
+    colorbaselightmain: '#EDEDED',
+    colorBaselighthightlight: '#D6D5D5',
+    colorBaseLightContrast: '#FFFFFF',
+
+
+  },
+
+  Backgroundcolors:{
+    colorcackground01:'#FFFFFF'
+  },
+
+};
